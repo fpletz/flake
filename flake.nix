268c205701c4d7791617032152fcbc86f38ad7bd
Franz Pletz <fpletz@fnordicwalking.de>
2023-08-09 21:02:18 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Added input 'home-manager':
        'github:nix-community/home-manager/a8f8f48320c64bd4e3a266a850bbfde2c6fe3a04' (2023-08-09)
    • Added input 'home-manager/nixpkgs':
        follows 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
         nixpkgs-stable.follows = "nixpkgs";
       };
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {

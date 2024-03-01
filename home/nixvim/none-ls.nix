f7b9026d679b1a0e3f39f10d7acb0cbbf6d09060
Franz Pletz <fpletz@fnordicwalking.de>
2024-03-01 21:49:47 +0100

    nixvim: remove deprecated none-ls plugins

--- home/nixvim/none-ls.nix
+++ home/nixvim/none-ls.nix
@@ -5,17 +5,14 @@
     sources = {
       code_actions = {
         gitsigns.enable = true;
-        shellcheck.enable = true;
         statix.enable = true;
       };
       diagnostics = {
-        shellcheck.enable = true;
         deadnix.enable = true;
       };
       formatting = {
         isort.enable = true;
         nixpkgs_fmt.enable = true;
-        rustfmt.enable = true;
         shfmt.enable = true;
       };
     };

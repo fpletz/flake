1485c0cb5d5cf046256bc360030d139415e9e9bc
Franz Pletz <fpletz@fnordicwalking.de>
2024-03-08 03:10:32 +0100

    nixvim: update for cmp refactoring

--- home/nixvim/cmp.nix
+++ home/nixvim/cmp.nix
@@ -4,63 +4,45 @@
     colorschemes.catppuccin.integrations.cmp = true;
 
     plugins = {
-      nvim-cmp = {
+      cmp = {
         enable = true;
         autoEnableSources = false;
-        sources = [
-          {
-            name = "nvim_lsp";
-            groupIndex = 1;
-          }
-          {
-            name = "nvim_lsp_signature_help";
-            groupIndex = 1;
-          }
-          {
-            name = "treesitter";
-            groupIndex = 1;
-          }
-          {
-            name = "path";
-            groupIndex = 1;
-          }
-          {
-            name = "calc";
-            groupIndex = 1;
-          }
-          {
-            name = "emoji";
-            groupIndex = 1;
-          }
-          {
-            name = "luasnip";
-            groupIndex = 1;
-          }
-          {
-            name = "buffer";
-            keywordLength = 3;
-            groupIndex = 2;
-          }
-          {
-            name = "rg";
-            keywordLength = 3;
-            groupIndex = 2;
-          }
-          {
-            name = "spell";
-            keywordLength = 3;
-            groupIndex = 3;
-          }
-        ];
-        mapping = {
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<CR>" = "cmp.mapping.confirm({ select = false })";
+        settings = {
+          sources.__raw = ''
+            cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'nvim_lsp_signature_help' },
+              { name = 'treesitter' },
+              { name = 'path' },
+              { name = 'luasnip' },
+            }, {
+              { name = 'buffer', keywordLength = 3 },
+              { name = 'calc' },
+              { name = 'emoji' },
+            }, {
+              { name = 'rg', keywordLength = 3 },
+              { name = 'spell', keywordLength = 3 },
+            })
+          '';
+          mapping = {
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<CR>" = "cmp.mapping.confirm({ select = false })";
+          };
+          mappingPresets = [ "insert" ];
+          snippet.expand = "luasnip";
         };
-        mappingPresets = [ "insert" ];
-        snippet.expand = "luasnip";
+        filetype.gitcommit.sources.__raw = ''
+          cmp.config.sources({
+            { name = 'git' },
+            { name = 'path' },
+            { name = 'emoji' },
+          }, {
+            { name = 'buffer', keyword_length = 3 },
+          })
+        '';
       };
       cmp-buffer.enable = true;
       cmp-calc.enable = true;
@@ -102,16 +84,6 @@
 
     extraConfigLua = ''
       local cmp = require('cmp')
-
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'git' },
-          { name = 'path' },
-          { name = 'emoji' },
-        }, {
-          { name = 'buffer', keyword_length = 3 },
-        })
-      })
 
       cmp.setup.cmdline('/', {
         mapping = cmp.mapping.preset.cmdline(),

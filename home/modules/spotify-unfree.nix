0d4e0ec5febb5a65b001e0428cd3f847824e3c17
Franz Pletz <fpletz@fnordicwalking.de>
2024-06-13 00:39:04 +0200

    spotify-unfree: enable if gui

--- home/modules/spotify-unfree.nix
+++ home/modules/spotify-unfree.nix
@@ -41,7 +41,7 @@ in
   options.programs.spotify-unfree = {
     enable = lib.mkOption {
       type = lib.types.bool;
-      default = config.bpletza.workstation.sway && pkgs.system == "x86_64-linux";
+      default = config.bpletza.workstation.gui && pkgs.system == "x86_64-linux";
       description = "Unfree Spotify Client";
     };
   };

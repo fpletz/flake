bf8a942cbcc787e8ba6c8ede45525c35218bfafe
Franz Pletz <fpletz@fnordicwalking.de>
2024-06-28 11:50:43 +0200

    nvidia: add fbdev module option

--- nixos/workstation/nvidia.nix
+++ nixos/workstation/nvidia.nix
@@ -12,14 +12,19 @@ in
   options.bpletza.workstation.nvidia = mkEnableOption "nvidia support";
 
   config = mkIf cfg.nvidia {
-    nixpkgs.config.allowUnfree = true;
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
-      modesetting.enable = true;
-      forceFullCompositionPipeline = true;
+    nixpkgs.config = {
+      allowUnfree = true;
+      cudaSupport = true;
     };
-    hardware.graphics.extraPackages = [ pkgs.nvidia-vaapi-driver ];
-    nixpkgs.config.cudaSupport = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    boot.kernelParams = [ "nvidia-drm.fbdev=1" ];
+    hardware = {
+      nvidia = {
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        modesetting.enable = true;
+        forceFullCompositionPipeline = true;
+      };
+      graphics.extraPackages = [ pkgs.nvidia-vaapi-driver ];
+    };
   };
 }

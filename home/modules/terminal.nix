b869d5ec2dcb1f1051abec2928b9cfb3defe8610
Franz Pletz <fpletz@fnordicwalking.de>
2024-06-30 01:02:07 +0200

    terminal: remove wezterm

--- home/modules/terminal.nix
+++ home/modules/terminal.nix
@@ -44,37 +44,6 @@
       };
     };
 
-    programs.wezterm = {
-      enable = true;
-      extraConfig = ''
-        local wezterm = require 'wezterm'
-        local config = wezterm.config_builder()
-        config.scrollback_lines = 4000
-        config.font = wezterm.font_with_fallback {
-          'Fira Code',
-          'Noto Color Emoji',
-        }
-        config.harfbuzz_features = { 'ss02', 'ss04', 'ss05', 'cv19', 'cv23', 'ss09' }
-        config.font_size = 10
-        config.color_scheme = 'tokyonight_night'
-        config.hide_tab_bar_if_only_one_tab = true
-        config.check_for_updates = false
-        config.visual_bell = {
-          fade_in_function = 'EaseIn',
-          fade_in_duration_ms = 150,
-          fade_out_function = 'EaseOut',
-          fade_out_duration_ms = 150,
-        }
-        config.window_padding = {
-          left = 0,
-          right = 0,
-          top = 0,
-          bottom = 0,
-        }
-        return config
-      '';
-    };
-
     programs.kitty = {
       enable = true;
       font = {

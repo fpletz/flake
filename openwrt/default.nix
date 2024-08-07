dcdf7f0b9f45c7205a6a402ed0acc4c4e2246a1e
Franz Pletz <fpletz@fnordicwalking.de>
2024-08-07 13:54:36 +0200

    router: new snapshot build

--- openwrt/default.nix
+++ openwrt/default.nix
@@ -11,11 +11,10 @@
         in
         {
           router = buildOpenwrtImage "cudy_x6-v1" {
-            #release = "snapshot";
-            #sha256 = "sha256-18GQ3ouG5UJTSmdtw8bIr7goRHntUxIjXvCoX7vp9Sc=";
+            release = "snapshot";
+            sha256 = "sha256-zvV+NXmI3df9nHZo3HDRXZ5PPnrNRgrO7wcHfMEh7IA=";
             packages = [
               "luci"
-              "luci-app-tor"
               "luci-app-ddns"
               "luci-proto-wireguard"
               "luci-app-vnstat2"
@@ -29,6 +28,7 @@
               "prometheus-node-exporter-lua-wifi"
               "prometheus-node-exporter-lua-wifi_stations"
               "tcpdump"
+              "htop"
             ];
           };
         };

97a141e2cdf399bde5b2b7f153b43c5f775dee4f
Franz Pletz <fpletz@fnordicwalking.de>
2024-08-10 00:48:48 +0200

    nixvim/trouble: fix keybinds

--- home/nixvim/trouble.nix
+++ home/nixvim/trouble.nix
@@ -4,32 +4,23 @@
       {
         mode = "n";
         key = "<leader>xx";
-        action.__raw = "function() require(\"trouble\").toggle() end";
-      }
-      {
-        mode = "n";
-        key = "<leader>xw";
-        action.__raw = "function() require(\"trouble\").toggle(\"workspace_diagnostics\") end";
-      }
-      {
-        mode = "n";
-        key = "<leader>xd";
-        action.__raw = "function() require(\"trouble\").toggle(\"document_diagnostics\") end";
+        action.__raw = ''
+          function() require("trouble").toggle("diagnostics") end
+        '';
       }
       {
         mode = "n";
         key = "<leader>xq";
-        action.__raw = "function() require(\"trouble\").toggle(\"quickfix\") end";
+        action.__raw = ''
+          function() require("trouble").toggle("quickfix") end
+        '';
       }
       {
         mode = "n";
         key = "<leader>xl";
-        action.__raw = "function() require(\"trouble\").toggle(\"loclist\") end";
-      }
-      {
-        mode = "n";
-        key = "gR";
-        action.__raw = "function() require(\"trouble\").toggle(\"lsp_references\") end";
+        action.__raw = ''
+          function() require("trouble").toggle("loclist") end
+        '';
       }
     ];
     plugins = {

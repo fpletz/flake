c7136ebb1fa45138ced05b122a185faf9ef812c5
Franz Pletz <fpletz@fnordicwalking.de>
2024-08-12 14:06:37 +0200

    pkgs: override sway/wlroots to support explicit sync

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,14 +15,44 @@
         inherit (pkgs) callPackage;
         directory = ./by-name;
       };
+
+      legacyPackages = pkgs;
     };
 
   flake.overlays.default =
-    final: _prev:
+    final: prev:
     {
       linuxPackages-xanmod = (final.linuxPackagesFor final.linux-xanmod).extend (
         final: _prev: { ryzen_smu = final.callPackage ./ryzen_smu.nix { }; }
       );
+      sway-unwrapped = prev.sway-unwrapped.overrideAttrs (attrs: {
+        version = "0-unstable-2024-08-11";
+        src = final.fetchFromGitHub {
+          owner = "swaywm";
+          repo = "sway";
+          rev = "b44015578a3d53cdd9436850202d4405696c1f52";
+          hash = "sha256-gTsZWtvyEMMgR4vj7Ef+nb+wcXkwGivGfnhnBIfPHOA=";
+        };
+        buildInputs = attrs.buildInputs ++ [ final.wlroots ];
+        mesonFlags =
+          let
+            inherit (lib.strings) mesonEnable mesonOption;
+          in
+          [
+            (mesonOption "sd-bus-provider" "libsystemd")
+            (mesonEnable "tray" attrs.trayEnabled)
+          ];
+      });
+      wlroots = prev.wlroots.overrideAttrs (_attrs: {
+        version = "0-unstable-2024-08-11";
+        src = final.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "wlroots";
+          repo = "wlroots";
+          rev = "6214144735b6b85fa1e191be3afe33d6bea0faee";
+          hash = "sha256-nuG2xXLDFsGh23CnhaTtdOshCBN/yILqKCSmqJ53vgI=";
+        };
+      });
     }
     // lib.filesystem.packagesFromDirectoryRecursive {
       inherit (final) callPackage;

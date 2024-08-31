6597b3fdba89ae047e47403f88fde3770a89f0a5
Franz Pletz <fpletz@fnordicwalking.de>
2024-08-31 20:45:39 +0200

    workstation: pipewire bluetooth enhancements

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -253,6 +253,19 @@ in
       alsa.support32Bit = config.nixpkgs.system == "x86_64-linux";
       pulse.enable = true;
       jack.enable = true;
+      wireplumber.extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+          ];
+        };
+      };
     };
 
     services.upower = {

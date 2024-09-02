6433ac00a1e15be894b7620c5e259f629e591932
Franz Pletz <fpletz@fnordicwalking.de>
2024-09-02 17:32:56 +0200

    flake: ugprade lix to 2.91.0

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     flake-compat.url = "github:edolstra/flake-compat";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

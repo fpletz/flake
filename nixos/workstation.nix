6dbb64ab1ae8072bf632d794553f25e7362d020d
Franz Pletz <fpletz@fnordicwalking.de>
2024-09-14 14:12:40 +0200

    workstation: remove explicit bluetooth roles

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -258,12 +258,6 @@ in
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-          ];
         };
       };
       extraConfig = {

f7867037fbb66635dc2ecbd9b355edbd42c4ea06
Franz Pletz <fpletz@fnordicwalking.de>
2024-11-04 19:22:04 +0100

    pkgs: remove sway & wlroots git overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -20,38 +20,9 @@
     };
 
   flake.overlays.default =
-    final: prev:
+    final: _prev:
     {
       linuxPackages-xanmod = final.linuxPackagesFor final.linux-xanmod;
-      opencv = prev.opencv.override { enableCuda = false; };
-      sway-unwrapped = prev.sway-unwrapped.overrideAttrs (attrs: {
-        version = "0-unstable-2024-10-20";
-        src = final.fetchFromGitHub {
-          owner = "swaywm";
-          repo = "sway";
-          rev = "a63027245a6805bb952e47c5751ecdd7d1063d2f";
-          hash = "sha256-5/1oVqORWTYHWpxMhgV+n1ORE2d7YkMtVjdtw9KgQ+U=";
-        };
-        buildInputs = attrs.buildInputs ++ [ final.wlroots ];
-        mesonFlags =
-          let
-            inherit (lib.strings) mesonEnable mesonOption;
-          in
-          [
-            (mesonOption "sd-bus-provider" "libsystemd")
-            (mesonEnable "tray" attrs.trayEnabled)
-          ];
-      });
-      wlroots = prev.wlroots.overrideAttrs (_attrs: {
-        version = "0-unstable-2024-10-15";
-        src = final.fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "wlroots";
-          repo = "wlroots";
-          rev = "47fb00f66d5a8367d0108bd960f99e51ab1a1318";
-          hash = "sha256-HkpTML14tbYBLiHkqoFRSn/qKsVny/oso2TuG6LY/fk=";
-        };
-      });
     }
     // lib.filesystem.packagesFromDirectoryRecursive {
       inherit (final) callPackage;

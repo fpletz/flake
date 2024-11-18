8d6cd55e06ec58a659e2907c9f1db18637db21cc
Franz Pletz <fpletz@fnordicwalking.de>
2024-11-19 00:59:23 +0100

    linux-xanmod: 6.11.8 -> 6.11.9

--- pkgs/by-name/linux-xanmod.nix
+++ pkgs/by-name/linux-xanmod.nix
@@ -9,8 +9,8 @@
 let
   suffix = "xanmod1";
   modDirVersion = lib.versions.pad 3 "${version}-${suffix}";
-  version = "6.11.8";
-  hash = "sha256-ytn4b230zVGEbdQwQwSDOVMdLbDJWVd6v/BnR76dEt4=";
+  version = "6.11.9";
+  hash = "sha256-lR7GXFy3eYq75+LwVlXScPYWbdVW6wAV+He0YZ+5AF4=";
 in
 buildLinux {
   inherit version modDirVersion;

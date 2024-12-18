28b504e748e95e5030e41b4fff25e84295add8c6
Franz Pletz <fpletz@fnordicwalking.de>
2024-12-19 00:21:04 +0100

    zram: give zram more priority, optimized sysctls

--- nixos/default/zram.nix
+++ nixos/default/zram.nix
@@ -7,9 +7,11 @@
 
   boot = {
     tmp.tmpfsSize = "200%";
-
     kernel.sysctl = {
-      "vm.swappiness" = 100;
+      "vm.swappiness" = 150;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
     };
   };
 }

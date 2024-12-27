152a69ca6af438f0e35417026a8dff816702883e
Franz Pletz <fpletz@fnordicwalking.de>
2024-12-27 14:16:49 +0100

    home/nixvim: don't install broken nix treesitter grammar :/

--- home/nixvim/treesitter.nix
+++ home/nixvim/treesitter.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   programs.nixvim = {
     colorschemes.catppuccin.settings.integrations.treesitter = true;
@@ -6,8 +7,94 @@
       treesitter = {
         enable = true;
         nixvimInjections = true;
+
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          awk
+          bash
+          bibtex
+          c
+          cmake
+          comment
+          commonlisp
+          cpp
+          css
+          csv
+          dhall
+          diff
+          disassembly
+          dockerfile
+          dot
+          dtd
+          elixir
+          erlang
+          fish
+          git-config
+          git-rebase
+          gitcommit
+          gitignore
+          gleam
+          go
+          gomod
+          gosum
+          gotmpl
+          graphql
+          haskell
+          hcl
+          helm
+          html
+          http
+          java
+          javascript
+          json
+          json5
+          just
+          liquidsoap
+          lua
+          make
+          markdown
+          markdown-inline
+          meson
+          ninja
+          # nix
+          objdump
+          org
+          passwd
+          pem
+          perl
+          php
+          printf
+          promql
+          proto
+          python
+          regex
+          requirements
+          robots
+          sql
+          ssh-config
+          strace
+          systemtap
+          tcl
+          terraform
+          tmux
+          todotxt
+          toml
+          typescript
+          udev
+          vim
+          vimdoc
+          xml
+          yaml
+          zathurarc
+          zig
+        ];
         settings = {
-          highlight.enable = true;
+          highlight = {
+            enable = true;
+            additional_vim_regex_highlighting = true;
+            disable = [
+              "nix"
+            ];
+          };
           indent.enable = true;
           incremental_selection.enable = true;
         };

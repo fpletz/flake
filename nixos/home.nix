92df965dc0dde5e0177ad32f37b74633a48b4857
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-01 06:28:10 +0100

    nixos/home: create proper module

--- nixos/home.nix
+++ nixos/home.nix
@@ -4,60 +4,78 @@
   config,
   ...
 }:
+let
+  cfg = config.bpletza.home;
+in
 {
-  sops.secrets.fpletz-password.neededForUsers = true;
+  options.bpletza.home = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "home management for user";
+    };
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "fpletz";
+      description = "username";
+    };
+  };
 
   imports = [ inputs.home-manager.nixosModules.default ];
 
-  home-manager = lib.mkIf (!config.boot.isContainer) {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.fpletz = ../home/fpletz.nix;
-    extraSpecialArgs = {
-      inherit inputs;
-    };
-  };
+  config = lib.mkIf cfg.enable {
+    sops.secrets."${cfg.user}-password".neededForUsers = true;
 
-  users = {
-    users.fpletz = {
-      uid = 1000;
-      isNormalUser = true;
-      hashedPasswordFile = config.sops.secrets.fpletz-password.path;
-      group = "users";
-      extraGroups = [
-        "wheel"
-        "libvirtd"
-        "audio"
-        "video"
-        "docker"
-        "sway"
-        "wireshark"
-        "adbusers"
-        "input"
-        "podman"
-        "systemd-journal"
-      ];
-      home = "/home/fpletz";
-      shell = "/run/current-system/sw/bin/zsh";
-      linger = lib.mkDefault true;
-      subGidRanges = [
-        {
-          count = 65536;
-          startGid = 100001;
-        }
-      ];
-      subUidRanges = [
-        {
-          count = 65536;
-          startUid = 100001;
-        }
-      ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK20Lv3TggAXcctelNGBxjcQeMB4AqGZ1tDCzY19xBUV"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/VM56N9OsG/hK7LEwheHwptClBNPdBl/tIW8URWyQPsE0dN2FYAERsHom3I3IvAS3phfhYtLOwrQ+MqEt7u5f/E3CgdfvEFRER12arxlT/q3gSh5rUdq508fTjkUNmJr6Vul+BCZ7VeESa2yvvTesFqvdVP9NtpGbAusX/JCrXwQciygJ0hDuMdLFW8MmRzljDoBsyjz18MDaMzsGQddQuE+3uAzJ1NXZpNh+M+C6eLNe+QJQMb9VTPGB3Pc0cU0GWyXYpWTVkpJqJVe180ldMU9x2c2sBBcRM3N/UDn2MF3XQi3TdGO93AIcUHNCLmUvIdqz+DPdKzCt3c3HvHh"
-      ];
+    home-manager = lib.mkIf (!config.boot.isContainer) {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      users.${cfg.user} = ../home/${cfg.user}.nix;
+      extraSpecialArgs = {
+        inherit inputs;
+      };
     };
-  };
 
-  nix.settings.trusted-users = [ "fpletz" ];
+    users = {
+      users.${cfg.user} = {
+        uid = 1000;
+        isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets."${cfg.user}-password".path;
+        group = "users";
+        extraGroups = [
+          "wheel"
+          "libvirtd"
+          "audio"
+          "video"
+          "docker"
+          "sway"
+          "wireshark"
+          "adbusers"
+          "input"
+          "podman"
+          "systemd-journal"
+        ];
+        home = "/home/${cfg.user}";
+        shell = "/run/current-system/sw/bin/zsh";
+        linger = lib.mkDefault true;
+        subGidRanges = [
+          {
+            count = 65536;
+            startGid = 100001;
+          }
+        ];
+        subUidRanges = [
+          {
+            count = 65536;
+            startUid = 100001;
+          }
+        ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK20Lv3TggAXcctelNGBxjcQeMB4AqGZ1tDCzY19xBUV"
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/VM56N9OsG/hK7LEwheHwptClBNPdBl/tIW8URWyQPsE0dN2FYAERsHom3I3IvAS3phfhYtLOwrQ+MqEt7u5f/E3CgdfvEFRER12arxlT/q3gSh5rUdq508fTjkUNmJr6Vul+BCZ7VeESa2yvvTesFqvdVP9NtpGbAusX/JCrXwQciygJ0hDuMdLFW8MmRzljDoBsyjz18MDaMzsGQddQuE+3uAzJ1NXZpNh+M+C6eLNe+QJQMb9VTPGB3Pc0cU0GWyXYpWTVkpJqJVe180ldMU9x2c2sBBcRM3N/UDn2MF3XQi3TdGO93AIcUHNCLmUvIdqz+DPdKzCt3c3HvHh"
+        ];
+      };
+    };
+
+    nix.settings.trusted-users = [ cfg.user ];
+  };
 }

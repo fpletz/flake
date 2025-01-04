d3a578dbff53ca6435b7f44ea3bb684aed4a6bbf
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-04 13:23:55 +0100

    nixos/openssh: more hardening

--- nixos/default/openssh.nix
+++ nixos/default/openssh.nix
@@ -1,6 +1,8 @@
 {
   services.openssh = {
     enable = true;
+    startWhenNeeded = true;
+    openFirewall = true;
     hostKeys = [
       # only ed25519 hostkey, no rsa
       {
@@ -18,8 +20,14 @@
       PasswordAuthentication = false;
       PubkeyAuthOptions = "verify-required";
       # crypto hardening
-      Ciphers = [ "chacha20-poly1305@openssh.com" ];
-      KexAlgorithms = [ "curve25519-sha256@libssh.org" ];
+      Ciphers = [
+        "chacha20-poly1305@openssh.com"
+      ];
+      KexAlgorithms = [
+        "sntrup761x25519-sha512@openssh.com"
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+      ];
       Macs = [
         "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
@@ -28,5 +36,34 @@
       # no reverse dns
       UseDns = false;
     };
+  };
+
+  programs.ssh = {
+    ciphers = [
+      "chacha20-poly1305@openssh.com"
+      "aes256-gcm@openssh.com"
+    ];
+    kexAlgorithms = [
+      "sntrup761x25519-sha512@openssh.com"
+      "curve25519-sha256"
+      "curve25519-sha256@libssh.org"
+      "diffie-hellman-group-exchange-sha256"
+    ];
+    hostKeyAlgorithms = [
+      "ssh-ed25519-cert-v01@openssh.com"
+      "ssh-ed25519"
+      "sk-ssh-ed25519-cert-v01@openssh.com"
+      "sk-ssh-ed25519@openssh.com"
+      "ssh-rsa-cert-v01@openssh.com"
+      "ssh-rsa"
+    ];
+    macs = [
+      "hmac-sha2-512-etm@openssh.com"
+      "hmac-sha2-256-etm@openssh.com"
+      "umac-128-etm@openssh.com"
+      "hmac-sha2-512"
+      "hmac-sha2-256"
+      "umac-128@openssh.com"
+    ];
   };
 }

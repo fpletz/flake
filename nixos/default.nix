5010d14b8cc44b03dc205b2e9a3aaf23f0c34794
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-21 02:31:09 +0100

    default: add german locale for some settings

--- nixos/default.nix
+++ nixos/default.nix
@@ -33,7 +33,24 @@
 
   time.timeZone = "UTC";
   console.keyMap = "us";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF8";
+      LC_IDENTIFICATION = "de_DE.UTF8";
+      LC_MEASUREMENT = "de_DE.UTF8";
+      LC_MONETARY = "de_DE.UTF8";
+      LC_NAME = "de_DE.UTF8";
+      LC_NUMERIC = "de_DE.UTF8";
+      LC_PAPER = "de_DE.UTF8";
+      LC_TELEPHONE = "de_DE.UTF8";
+      LC_TIME = "de_DE.UTF8";
+    };
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+    ];
+  };
 
   boot = {
     enableContainers = false;

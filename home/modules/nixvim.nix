d1cbb6d9db799c0f1c0a004e24fd5a6b2d37647a
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-22 04:49:59 +0100

    home/nixvim: add vim nvim alias

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -19,10 +19,9 @@
       EDITOR = "nvim";
     };
 
-    programs = {
-      bash.shellAliases.vimdiff = "nvim -d";
-      fish.shellAliases.vimdiff = "nvim -d";
-      zsh.shellAliases.vimdiff = "nvim -d";
+    home.shellAliases = {
+      vim = "nvim";
+      vimdiff = "nvim -d";
     };
   };
 }

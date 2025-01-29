882e3022ffcea5d5c3ee4e71d9ea53b18b112471
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-29 10:35:43 +0100

    hardware/thinkpad-p14s-gen5: prevent ec to wake up system

--- nixos/hardware/thinkpad-p14s-gen5.nix
+++ nixos/hardware/thinkpad-p14s-gen5.nix
@@ -15,6 +15,8 @@
       # disable PSR to fix stutters
       # https://gitlab.freedesktop.org/drm/amd/-/issues/3647
       "amdgpu.dcdebugmask=0x10"
+      # Embedded controller causes battery drain in s2idle
+      "acpi.ec_no_wakeup=1"
     ];
 
     boot.initrd.availableKernelModules = [

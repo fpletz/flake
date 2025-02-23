9695ac21f48c68a350cd3ee956854b18ba4aee05
Franz Pletz <fpletz@fnordicwalking.de>
2025-02-23 13:27:52 +0100

    openssh: add new post-quantum kex algos

--- nixos/default/openssh.nix
+++ nixos/default/openssh.nix
@@ -24,6 +24,8 @@
         "chacha20-poly1305@openssh.com"
       ];
       KexAlgorithms = [
+        "mlkem768x25519-sha256"
+        "sntrup761x25519-sha512"
         "sntrup761x25519-sha512@openssh.com"
         "curve25519-sha256"
         "curve25519-sha256@libssh.org"
@@ -44,6 +46,8 @@
       "aes256-gcm@openssh.com"
     ];
     kexAlgorithms = [
+      "mlkem768x25519-sha256"
+      "sntrup761x25519-sha512"
       "sntrup761x25519-sha512@openssh.com"
       "curve25519-sha256"
       "curve25519-sha256@libssh.org"

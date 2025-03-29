b8e053fb82e5a0f28a1502b59afb52a7f554127e
Franz Pletz <fpletz@fnordicwalking.de>
2025-03-29 08:33:33 +0100

    Revert "workstation: remove x86 32bit compat"
    This reverts commit e9e839c24cd83e49bf2e30adf547af3ab453e4ed.

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -231,12 +231,14 @@ in
         pkgs.libvdpau-va-gl
         pkgs.vulkan-validation-layers
       ];
+      enable32Bit = pkgs.stdenv.hostPlatform.isx86_64 && cfg.gaming;
     };
 
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
+      alsa.support32Bit = pkgs.stdenv.hostPlatform.isx86_64 && cfg.gaming;
       pulse.enable = true;
       jack.enable = true;
       wireplumber.extraConfig.bluetoothEnhancements = {

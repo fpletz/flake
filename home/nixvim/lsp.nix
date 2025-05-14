3ef67fe8af06fd836b9928f42364c276c2b5eb5b
Franz Pletz <fpletz@fnordicwalking.de>
2025-05-14 05:18:44 +0200

    nixvim: lsp refactoring for vim.lsp

--- home/nixvim/lsp.nix
+++ home/nixvim/lsp.nix
@@ -8,7 +8,6 @@
 
   extraPlugins = with pkgs.vimPlugins; [
     nvim-surround
-    pest-vim
   ];
 
   extraConfigLua = ''
@@ -44,91 +43,102 @@
     }
   ];
 
-  plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        bashls.enable = true;
-        clangd.enable = true;
-        cmake.enable = true;
-        cssls.enable = true;
-        docker_compose_language_service.enable = true;
-        dockerls.enable = true;
-        gitlab_ci_ls.enable = true;
-        gopls.enable = true;
-        html.enable = true;
-        htmx.enable = true;
-        jsonls.enable = true;
-        lemminx.enable = true;
-        lua_ls.enable = true;
-        nixd = {
-          enable = true;
-          extraOptions = {
-            offset_encoding = "utf-8";
-          };
-          settings =
-            let
-              localFlake = ''(builtins.getFlake "/home/fpletz/src/flake")'';
-            in
-            {
-              nixpkgs.expr = "import <nixpkgs> {}";
-              formatting.command = [ (lib.getExe pkgs.nixfmt-rfc-style) ];
-              options = {
-                nixos.expr = "${localFlake}.nixosConfigurations.server.options";
-                home-manager.expr = "${localFlake}.homeConfigurations.fpletz.options";
-              };
+  lsp = {
+    servers = {
+      bashls.enable = true;
+      clangd.enable = true;
+      cmake.enable = true;
+      cssls.enable = true;
+      docker_compose_language_service.enable = true;
+      dockerls.enable = true;
+      gitlab_ci_ls.enable = true;
+      gopls.enable = true;
+      html.enable = true;
+      htmx.enable = true;
+      jsonls.enable = true;
+      lemminx.enable = true;
+      lua_ls.enable = true;
+      nixd = {
+        enable = true;
+        settings =
+          let
+            localFlake = ''(builtins.getFlake "/home/fpletz/src/flake")'';
+          in
+          {
+            nixpkgs.expr = "import <nixpkgs> {}";
+            formatting.command = [ (lib.getExe pkgs.nixfmt-rfc-style) ];
+            options = {
+              nixos.expr = "${localFlake}.nixosConfigurations.server.options";
+              home-manager.expr = "${localFlake}.homeConfigurations.fpletz.options";
             };
-        };
-        marksman.enable = true;
-        pest_ls = {
-          enable = true;
-          onAttach.function = ''
-            require("pest-vim").setup()
-          '';
-        };
-        pyright.enable = true;
-        ruff.enable = true;
-        taplo.enable = true;
-        texlab.enable = true;
-        yamlls.enable = true;
-        zls.enable = true;
+          };
       };
-      keymaps = {
-        diagnostic = {
-          "<leader>j" = "goto_next";
-          "<leader>k" = "goto_prev";
-        };
-        lspBuf = {
-          K = "hover";
-          "<C-k>" = "signature_help";
-          gD = "references";
-          gd = "definition";
-          gi = "implementation";
-          gt = "type_definition";
-          rn = "rename";
-          ca = "code_action";
-        };
-        silent = true;
-        extra = [
-          {
-            action.__raw = "function() require('telescope.builtin').lsp_references() end";
-            key = "gD";
-          }
-          {
-            action.__raw = "function() require('telescope.builtin').lsp_definitions() end";
-            key = "gd";
-          }
-          {
-            action.__raw = "function() require('telescope.builtin').lsp_implementations() end";
-            key = "gi";
-          }
-          {
-            action.__raw = "function() require('telescope.builtin').lsp_type_definitions() end";
-            key = "gt";
-          }
-        ];
-      };
+      marksman.enable = true;
+      pyright.enable = true;
+      ruff.enable = true;
+      taplo.enable = true;
+      texlab.enable = true;
+      yamlls.enable = true;
+      zls.enable = true;
     };
+    keymaps = [
+      {
+        key = "gd";
+        lspBufAction = "definition";
+      }
+      {
+        key = "gD";
+        lspBufAction = "references";
+      }
+      {
+        key = "gt";
+        lspBufAction = "type_definition";
+      }
+      {
+        key = "gi";
+        lspBufAction = "implementation";
+      }
+      {
+        key = "K";
+        lspBufAction = "hover";
+      }
+      {
+        key = "<leader>k";
+        action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
+      }
+      {
+        key = "<leader>j";
+        action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
+      }
+      {
+        key = "<leader>lx";
+        action = "<CMD>LspStop<Enter>";
+      }
+      {
+        key = "<leader>ls";
+        action = "<CMD>LspStart<Enter>";
+      }
+      {
+        key = "<leader>lr";
+        action = "<CMD>LspRestart<Enter>";
+      }
+      {
+        key = "gd";
+        action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_definitions";
+      }
+      {
+        key = "rn";
+        lspBufAction = "rename";
+      }
+      {
+        key = "ca";
+        lspBufAction = "code_action";
+      }
+    ];
+  };
+
+  plugins = {
+    lsp.enable = true;
     lsp-lines.enable = true;
     lsp-format.enable = true;
     rustaceanvim = {

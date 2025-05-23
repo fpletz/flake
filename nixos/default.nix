f724db2011c402c4289fee7e785e087c5bd2d9ac
Franz Pletz <fpletz@fnordicwalking.de>
2025-05-23 17:50:09 +0200

    nixos/default: resolve current system symlink in nvd diff

--- nixos/default.nix
+++ nixos/default.nix
@@ -122,7 +122,8 @@
     activationScripts.preActivation = ''
       if [[ -e /run/current-system ]]; then
         echo "--- diff to current-system"
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
+        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff \
+          "$(readlink -f /run/current-system)" "$systemConfig"
         echo "---"
       fi
     '';

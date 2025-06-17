3d66f9a7d85e969dea2e0e0860530bedb0754af6
Franz Pletz <fpletz@fnordicwalking.de>
2025-06-17 19:14:58 +0200

    nixos: fix default locales

--- nixos/default.nix
+++ nixos/default.nix
@@ -40,19 +40,20 @@
   console.keyMap = "us";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF8";
-      LC_IDENTIFICATION = "de_DE.UTF8";
-      LC_MEASUREMENT = "de_DE.UTF8";
-      LC_MONETARY = "de_DE.UTF8";
-      LC_NAME = "de_DE.UTF8";
-      LC_PAPER = "de_DE.UTF8";
-      LC_TELEPHONE = "de_DE.UTF8";
-    };
-    supportedLocales = [
+    extraLocales = [
       "en_US.UTF-8/UTF-8"
       "de_DE.UTF-8/UTF-8"
     ];
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "C.UTF-8";
+    };
   };
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";

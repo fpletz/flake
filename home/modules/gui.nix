c0702474eeb6c646ee5782029e09c4b432364c9e
Franz Pletz <fpletz@fnordicwalking.de>
2025-07-09 17:26:46 +0200

    home/gui: set dark mode using freedesktop dconf scheme

--- home/modules/gui.nix
+++ home/modules/gui.nix
@@ -40,6 +40,9 @@ in
       "org/gnome/desktop/interface" = {
         "color-scheme" = "prefer-dark";
       };
+      "org/freedesktop/appearance" = {
+        "color-scheme" = 1;
+      };
     };
 
     gtk = {

4cd24ea52f6f4366b8bb04128ee00723b10954f9
Franz Pletz <fpletz@fnordicwalking.de>
2025-07-29 11:47:25 +0200

    nixos/zram: decrease swappiness

--- nixos/default/zram.nix
+++ nixos/default/zram.nix
@@ -9,7 +9,7 @@
   boot = {
     tmp.tmpfsSize = "200%";
     kernel.sysctl = {
-      "vm.swappiness" = 150;
+      "vm.swappiness" = 100;
       "vm.watermark_boost_factor" = 0;
       "vm.watermark_scale_factor" = 125;
       "vm.page-cluster" = 0;

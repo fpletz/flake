4d24dec5ddfb5b1548b6ff3ce9d20fbaff3bd3ed
Franz Pletz <fpletz@fnordicwalking.de>
2025-08-08 18:02:34 +0200

    workstation: tuigreet is now in pkgs top-level

--- nixos/modules/workstation/wayland.nix
+++ nixos/modules/workstation/wayland.nix
@@ -11,7 +11,7 @@ in
   config = lib.mkIf cfg.enable {
     services.greetd = {
       enable = true;
-      settings.default_session.command = "${lib.getExe pkgs.greetd.tuigreet} --remember-session";
+      settings.default_session.command = "${lib.getExe pkgs.tuigreet} --remember-session";
     };
 
     xdg.portal = {

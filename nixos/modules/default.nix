b2347a3fbf274bc849b44459bcbf92a18b9ac41b
Franz Pletz <fpletz@fnordicwalking.de>
2025-10-04 19:28:46 +0200

    nixos: libvirtd ovmfs are now available by default

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -181,7 +181,6 @@
     libvirtd = {
       qemu = {
         swtpm.enable = true;
-        ovmf.packages = lib.mkForce [ pkgs.OVMFFull.fd ];
       };
       onShutdown = lib.mkDefault "shutdown";
       onBoot = lib.mkDefault "ignore";

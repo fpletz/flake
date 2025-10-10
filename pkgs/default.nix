3e2fc39aea409ea9d8396225d4d92959c4b1bfd1
Franz Pletz <fpletz@fnordicwalking.de>
2025-10-10 08:13:36 +0200

    pkgs/sway: unstable-2025-10-05

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -45,6 +45,27 @@ in
         withKeePassX11 = false;
         withKeePassYubiKey = false;
       };
+      wlroots_0_19 = prev.wlroots_0_19.overrideAttrs (
+        { src, ... }:
+        {
+          version = "0.19.2-unstable-2025-10-05";
+          src = src.override {
+            rev = "5529aae3e65aa182d88cecc1efa4b10a20b553eb";
+            hash = "sha256-pvc+SzNe1twhuaSzRUmiuhEQq7d3gYFgilrMlVPsguU=";
+          };
+        }
+      );
+      sway-unwrapped = prev.sway-unwrapped.overrideAttrs (
+        { src, ... }:
+        {
+          version = "0.12-unstable-2025-10-05";
+          src = src.override {
+            rev = "90d3270970cc963454455b572883a051d3f376a1";
+            hash = "sha256-X/JYV5lBfi4xnRA8d8bk/+DTaC2UWasNOBz3KYFMFUU=";
+          };
+        }
+      );
+
       linuxPackages-xanmod = final.linuxPackagesFor final.linux-xanmod;
     }
     // byNamePackages final;

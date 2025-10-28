9eb974cbc3decba26c8ac1dd9f6f3e0ad3c71f57
Franz Pletz <fpletz@fnordicwalking.de>
2025-10-28 21:42:30 +0100

    zocknix: add muccc vpn

--- machines/zocknix.nix
+++ machines/zocknix.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 
   networking.hostName = "zocknix";
 
@@ -109,5 +109,63 @@
     ai = true;
     ytdlVideoCodec = "av01";
     ytdlMaxRes = 1440;
+  };
+
+  sops = {
+    secrets = {
+      wg-muccc-private = {
+        sopsFile = ../secrets/zocknix.yaml;
+        owner = "systemd-network";
+      };
+      wg-muccc-psk = {
+        sopsFile = ../secrets/zocknix.yaml;
+        owner = "systemd-network";
+      };
+    };
+  };
+
+  systemd.network = {
+    netdevs."10-muccc" = {
+      netdevConfig = {
+        Name = "muccc";
+        Kind = "wireguard";
+        MTUBytes = 1300;
+      };
+      wireguardConfig = {
+        PrivateKeyFile = config.sops.secrets.wg-muccc-private.path;
+      };
+      wireguardPeers = [
+        {
+          PublicKey = "GnJFf9goxF/1IjMFTYJhi2ZcV8BREgpfK1+S7Wv/cxY=";
+          Endpoint = "stammstrecke.muc.ccc.de:51820";
+          PersistentKeepalive = 30;
+          PresharedKeyFile = config.sops.secrets.wg-muccc-psk.path;
+          AllowedIPs = [
+            "10.189.0.0/16"
+            "2a01:7e01:e003:8b00::/56"
+          ];
+        }
+      ];
+    };
+    networks."10-muccc" = {
+      matchConfig.Name = "muccc";
+      networkConfig = {
+        Address = [
+          "10.189.10.3/32"
+          "2a01:7e01:e003:8b10::3/128"
+        ];
+        DNS = [ "2a01:7e01:e003:8b00::53" ];
+        DNSOverTLS = false;
+        Domains = [ "club.muc.ccc.de" ];
+      };
+      routes = [
+        {
+          Destination = "10.189.0.0/16";
+        }
+        {
+          Destination = "2a01:7e01:e003:8b00::/56";
+        }
+      ];
+    };
   };
 }

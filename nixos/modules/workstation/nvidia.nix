55aec99efd0f70e3f76f8385683f1d596663af09
Franz Pletz <fpletz@fnordicwalking.de>
2026-02-08 19:59:59 +0100

    nixos/nvidia: add cuda binary cache

--- nixos/modules/workstation/nvidia.nix
+++ nixos/modules/workstation/nvidia.nix
@@ -32,6 +32,15 @@ in
       };
     };
 
+    nix.settings = {
+      substituters = [
+        "https://cache.nixos-cuda.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      ];
+    };
+
     services.xserver.videoDrivers = [ "nvidia" ];
 
     boot = {

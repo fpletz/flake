63b8887139dbd17591270e9299720a98ed4302a7
Franz Pletz <fpletz@fnordicwalking.de>
2026-02-27 00:07:47 +0100

    nvidia: fix build for kernel 6.19

--- nixos/modules/workstation/nvidia.nix
+++ nixos/modules/workstation/nvidia.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -53,7 +54,23 @@ in
 
     hardware = {
       nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        package =
+          let
+            base = config.boot.kernelPackages.nvidiaPackages.latest;
+            cachyos-nvidia-patch = pkgs.fetchpatch {
+              url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+              sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+            };
+
+            # Patch the appropriate driver based on config.hardware.nvidia.open
+            driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+          in
+          base
+          // {
+            ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+              patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+            });
+          };
         modesetting.enable = true;
         forceFullCompositionPipeline = true;
         open = true;

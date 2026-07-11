4ea2f05f31c0a58fbcfc493e734fc3dbc5870da7
Franz Pletz <fpletz@fnordicwalking.de>
2026-07-11 14:34:42 +0200

    vulkan-validation-layers: remove build fix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -36,10 +36,6 @@ in
   flake.overlays.default =
     final: prev:
     {
-      vulkan-validation-layers = prev.vulkan-validation-layers.overrideAttrs (attrs: {
-        cmakeFlags = attrs.cmakeFlags ++ [ "-DUPDATE_DEPS=OFF" ];
-      });
-
       linuxPackages-xanmod = final.linuxPackagesFor final.linux-xanmod;
     }
     // byNamePackages final;
